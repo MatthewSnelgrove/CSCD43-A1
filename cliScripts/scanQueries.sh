eaac624fcb6c251d9ae5abbf9a49999136f98b6e
Matthew Snelgrove <matthew.s13542@gmail.com>
2025-02-11 17:13:34 -0500

    initial commit

---
+++ cliScripts/scanQueries.sh
@@ -0,0 +1,45 @@
+#!/bin/bash
+
+# Default values
+USE_INDEX_SCAN=false
+OUTPUT_FILE=""
+DIR_PATH="/home/matth/Winter2025/CSCD43/A1"
+SQL_FILE="$DIR_PATH/scripts/ScanQueries.sql"
+
+# Parse command line options
+while getopts "i" opt; do
+  case $opt in
+    i) 
+      # Flag to use index scan
+      USE_INDEX_SCAN=true
+      SQL_FILE="$DIR_PATH/scripts/IndexScanQueries.sql"
+      ;;
+    *)
+      echo "Usage: $0 [-i] output_filename"
+      exit 1
+      ;;
+  esac
+done
+
+# Shift positional parameters to get the output file name as the last argument
+shift $((OPTIND - 1))
+
+# Check if the output file name is provided as the last argument
+if [ -z "$1" ]; then
+  echo "Error: Output file name is required!"
+  echo "Usage: $0 [-i] output_filename"
+  exit 1
+fi
+
+# Set the output file to be in the ../outputs directory
+OUTPUT_FILE="$DIR_PATH/outputs/$1.txt"
+
+# Ensure the outputs directory exists
+mkdir -p "$DIR_PATH/outputs"
+
+# Run the PostgreSQL query file and output the result to the specified file
+psql -U postgres -d A1DB -f $SQL_FILE -o $OUTPUT_FILE
+
+# Notify the user that the script has finished
+echo "Queries have been executed and the output is saved to $OUTPUT_FILE"
+
